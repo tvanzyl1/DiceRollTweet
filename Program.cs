ab3d2a092b40a32488e30c97859447134384b8f3
tvanzyl1 <tvanzyl1@users.noreply.github.com>
2017-02-15 16:15:21 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace DiceRollTweet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new TwitterService(System.Configuration.ConfigurationManager.AppSettings["consumer"],
+                                            System.Configuration.ConfigurationManager.AppSettings["consumerSecret"]);
+            service.AuthenticateWith(System.Configuration.ConfigurationManager.AppSettings["token"],
+                                            System.Configuration.ConfigurationManager.AppSettings["tokenSecret"]);
+            while (true)
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(0, 100);
+                string str = String.Format("D4 : {0} \n" +
+                                            "D6 : {1} \n" +
+                                            "D8 : {2} \n" +
+                                            "D12 : {3} \n" +
+                                            "D20 : {4} \n",
+                                            random.Next(0, 4) + 1,
+                                            random.Next(0, 6) + 1,
+                                            random.Next(0, 8) + 1,
+                                            random.Next(0, 12) + 1,
+                                            random.Next(0, 20) + 1);
+                Console.WriteLine(str);
+
+                TwitterStatus result = service.SendTweet(new SendTweetOptions
+                {
+                    Status = str
+                });
+                Console.WriteLine(DateTime.Now.ToString() + "Tweet sent");
+                Thread.Sleep(120000);
+            }
+        }
+    }
+}
